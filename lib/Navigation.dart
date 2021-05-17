99990ade1b070369f78a96997149175fdd0c8e9c
HadenHiles <hadenhiles@gmail.com>
2021-05-17 15:12:05 -0400

    Add swipe to delete functionality to shooting session shots list. Fix session timer painting issue that interrupted swipe delete functionality every _onTick second.

--- lib/Navigation.dart
+++ lib/Navigation.dart
@@ -116,40 +116,40 @@ class _NavigationState extends State<Navigation> {
     return SessionServiceProvider(
       service: sessionService,
       child: Scaffold(
-        body: AnimatedBuilder(
-          animation: sessionService, // listen to ChangeNotifier
-          builder: (context, child) {
-            return SlidingUpPanel(
-              backdropEnabled: true,
-              controller: sessionPanelController,
-              maxHeight: MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top,
-              minHeight: sessionService.isRunning ? 65 : 0,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(10),
-                topRight: Radius.circular(10),
-              ),
-              onPanelOpened: () {
-                setState(() {
-                  _sessionPanelState = PanelState.OPEN;
-                });
-              },
-              onPanelClosed: () {
-                setState(() {
-                  _sessionPanelState = PanelState.CLOSED;
-                });
-              },
-              onPanelSlide: (double offset) {
-                setState(() {
-                  _bottomNavOffsetPercentage = offset;
-                });
-              },
-              panel: Container(
-                decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.primaryVariant,
-                ),
-                child: Column(
-                  children: [
-                    ListTile(
+        body: SlidingUpPanel(
+          backdropEnabled: true,
+          controller: sessionPanelController,
+          maxHeight: MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top,
+          minHeight: sessionService.isRunning ? 65 : 0,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(10),
+            topRight: Radius.circular(10),
+          ),
+          onPanelOpened: () {
+            setState(() {
+              _sessionPanelState = PanelState.OPEN;
+            });
+          },
+          onPanelClosed: () {
+            setState(() {
+              _sessionPanelState = PanelState.CLOSED;
+            });
+          },
+          onPanelSlide: (double offset) {
+            setState(() {
+              _bottomNavOffsetPercentage = offset;
+            });
+          },
+          panel: Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primaryVariant,
+            ),
+            child: Column(
+              children: [
+                AnimatedBuilder(
+                  animation: sessionService, // listen to ChangeNotifier
+                  builder: (context, child) {
+                    return ListTile(
                       tileColor: Theme.of(context).primaryColor,
                       title: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -199,46 +199,46 @@ class _NavigationState extends State<Navigation> {
                           });
                         }
                       },
-                    ),
-                    StartShooting(sessionPanelController: sessionPanelController),
-                  ],
+                    );
+                  },
                 ),
-              ),
-              body: NestedScrollView(
-                headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-                  return [
-                    SliverAppBar(
-                      collapsedHeight: 65,
-                      expandedHeight: 125,
-                      backgroundColor: HomeTheme.darkTheme.colorScheme.primary,
-                      iconTheme: Theme.of(context).iconTheme,
-                      actionsIconTheme: Theme.of(context).iconTheme,
-                      floating: true,
-                      pinned: true,
-                      flexibleSpace: DecoratedBox(
-                        decoration: BoxDecoration(
-                          color: HomeTheme.darkTheme.colorScheme.primaryVariant,
-                        ),
-                        child: FlexibleSpaceBar(
-                          collapseMode: CollapseMode.parallax,
-                          centerTitle: true,
-                          title: _title,
-                          background: Container(
-                            color: HomeTheme.darkTheme.colorScheme.primaryVariant,
-                          ),
-                        ),
+                StartShooting(sessionPanelController: sessionPanelController),
+              ],
+            ),
+          ),
+          body: NestedScrollView(
+            headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+              return [
+                SliverAppBar(
+                  collapsedHeight: 65,
+                  expandedHeight: 125,
+                  backgroundColor: HomeTheme.darkTheme.colorScheme.primary,
+                  iconTheme: Theme.of(context).iconTheme,
+                  actionsIconTheme: Theme.of(context).iconTheme,
+                  floating: true,
+                  pinned: true,
+                  flexibleSpace: DecoratedBox(
+                    decoration: BoxDecoration(
+                      color: HomeTheme.darkTheme.colorScheme.primaryVariant,
+                    ),
+                    child: FlexibleSpaceBar(
+                      collapseMode: CollapseMode.parallax,
+                      centerTitle: true,
+                      title: _title,
+                      background: Container(
+                        color: HomeTheme.darkTheme.colorScheme.primaryVariant,
                       ),
-                      actions: _actions,
                     ),
-                  ];
-                },
-                body: Container(
-                  padding: EdgeInsets.only(bottom: 0),
-                  child: _tabs.elementAt(_selectedIndex),
+                  ),
+                  actions: _actions,
                 ),
-              ),
-            );
-          },
+              ];
+            },
+            body: Container(
+              padding: EdgeInsets.only(bottom: 0),
+              child: _tabs.elementAt(_selectedIndex),
+            ),
+          ),
         ),
         bottomNavigationBar: SizedOverflowBox(
           alignment: AlignmentDirectional.topCenter,
