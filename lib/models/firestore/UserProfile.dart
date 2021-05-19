e5519f3f350afaecebf1c8b7dc370cdaec86dce8
HadenHiles <hadenhiles@gmail.com>
2021-05-19 15:09:54 -0400

    Fix the way display name is bootstrapped on login. Add email to userprofile model

--- lib/models/firestore/UserProfile.dart
+++ lib/models/firestore/UserProfile.dart
@@ -3,17 +3,22 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class UserProfile {
   String id;
   final String displayName;
+  final String email;
   DocumentReference reference;
 
-  UserProfile(this.displayName);
+  UserProfile(this.displayName, this.email);
 
   UserProfile.fromMap(Map<String, dynamic> map, {this.reference})
-      : assert(map['display_name'] != null),
-        id = map['id'],
-        displayName = map['display_name'];
+      : id = map['id'],
+        displayName = map['display_name'],
+        email = map['email'];
 
   Map<String, dynamic> toMap() {
-    return {'id': id, 'display_name': displayName};
+    return {
+      'id': id,
+      'display_name': displayName,
+      'email': email,
+    };
   }
 
   UserProfile.fromSnapshot(DocumentSnapshot snapshot) : this.fromMap(snapshot.data(), reference: snapshot.reference);
