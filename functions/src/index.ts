b9cc64685327b33cbb10d3baeb13ca448b6275fe
HadenHiles <hadenhiles@gmail.com>
2021-05-24 12:50:13 -0400

    Send invite notification to receiving user/teammate via cloud functions and firebase messaging. Try to force android to use app launcher icon as the notification icon

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,30 +8,20 @@ import * as admin from "firebase-admin";
 admin.initializeApp();
 
 export const inviteSent = functions.firestore.document("/invites/{userId}/invites/{teammateId}").onWrite(async (change, context) => {
-    let teammateMap;
+    let user;
+    let teammate;
     let teammateName;
-    let fcmToken;
+    let fcmToken: string | null;
 
     // Retrieve the user who will be receiving the notification
     await admin.firestore().collection("users").doc(context.params.userId).get().then(async (doc) => {
         functions.logger.log("Retrieved user with uid: " + doc.ref.id);
 
-        // Retrieve the teammate who sent the invite
-        await admin.firestore().collection("users").doc(context.params.teammateId).get().then((tDoc) => {
-            // Get the teammates name
-            teammateMap = tDoc.data();
-            teammateName = teammateMap != undefined ? teammateMap["display_name"] : "Someone";
-            fcmToken = teammateMap != undefined ? teammateMap["fcm_token"] : null;
-        });
-    }).catch((err) => {
-        functions.logger.log("Error fetching firestore users collection: " + err);
-        return null;
-    });
-
-    if (fcmToken != null) {
-        const data = {
+        user = doc.data();
+        fcmToken = user != undefined ? user.fcm_token : null;
+        let data = {
             "notification": {
-                "body": teammateName + " has sent you an invite",
+                "body": "Someone has sent you a teammate invite.",
                 "title": "Teammate challenge!",
             }, 
             "priority": "high", 
@@ -42,14 +32,36 @@ export const inviteSent = functions.firestore.document("/invites/{userId}/invite
             }, 
             "to": fcmToken,
         };
-    
-        request({
-            url: "https://fcm.googleapis.com/fcm/send",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `key=${functions.config().messagingservice.key}`,
-            },
-            body: JSON.stringify(data),
-        });
-    }
+
+        if (fcmToken != null) {
+            // Retrieve the teammate who sent the invite
+            await admin.firestore().collection("users").doc(context.params.teammateId).get().then((tDoc) => {
+                functions.logger.log("Retrieved teammate user with uid: " + tDoc.ref.id);
+
+                // Get the teammates name
+                teammate = tDoc.data();
+                teammateName = teammate != undefined ? teammate.display_name : "Someone";
+                data.notification.body = `${teammateName} has sent you an invite.`;
+
+                functions.logger.log("Sending notification with data: " + JSON.stringify(data));
+                
+                request({
+                    url: "https://fcm.googleapis.com/fcm/send",
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": `key=${functions.config().messagingservice.key}`,
+                    },
+                    body: JSON.stringify(data),
+                });
+            }).catch((err) => {
+                functions.logger.log("Error fetching firestore users (teammate) collection: " + err);
+            });
+        } else {
+            functions.logger.log("fcm_token not found");
+        }
+    }).catch((err) => {
+        functions.logger.log("Error fetching firestore users collection: " + err);
+        return null;
+    });
 });
