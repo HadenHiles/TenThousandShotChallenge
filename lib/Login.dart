7435bce6bc991d65dcfe4287063608ac2abec0d3
HadenHiles <haden@howtohockey.com>
2021-06-02 17:16:35 -0400

    Increment version for android release

--- lib/Login.dart
+++ lib/Login.dart
@@ -3,7 +3,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tenthousandshotchallenge/main.dart';
-import 'package:tenthousandshotchallenge/models/firestore/UserProfile.dart';
 import 'package:tenthousandshotchallenge/services/authentication/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
