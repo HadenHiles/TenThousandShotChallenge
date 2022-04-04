c4fe1c1b8c7f3378c51eb849c4c447aa884c695c
HadenHiles <hadenhiles@gmail.com>
2022-04-04 14:16:35 -0400

    Add test vimeo video (stream) to premium learn tab.

---
+++ lib/widgets/VideoStream.dart
@@ -0,0 +1,47 @@
+import 'package:chewie/chewie.dart';
+import 'package:flutter/material.dart';
+import 'package:tenthousandshotchallenge/theme/Theme.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoStream extends StatefulWidget {
+  VideoStream({Key key, this.url}) : super(key: key);
+
+  final String url;
+
+  @override
+  State<VideoStream> createState() => _VideoStreamState();
+}
+
+class _VideoStreamState extends State<VideoStream> {
+  VideoPlayerController _videoPlayerController;
+
+  @override
+  void initState() {
+    _videoPlayerController = VideoPlayerController.network(widget.url);
+    _videoPlayerController.initialize();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Chewie(
+      controller: ChewieController(
+        videoPlayerController: _videoPlayerController,
+        autoPlay: true,
+        looping: false,
+        allowFullScreen: true,
+        cupertinoProgressColors: ChewieProgressColors(
+          playedColor: Theme.of(context).primaryColor,
+          backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+          bufferedColor: lighten(Theme.of(context).primaryColor, 0.2),
+        ),
+      ),
+    );
+  }
+}
