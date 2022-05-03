95b45cf0dc4d054ee5b3b7777dac823806eb0568
hadenhiles <hi@hadenhiles.com>
2022-05-03 16:29:55 -0400

    Basic starting point for new "team" screen layout/bottom sheet "change team" functionality

--- lib/tabs/Team.dart
+++ lib/tabs/Team.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:tenthousandshotchallenge/theme/Theme.dart';
 
+const TEAM_HEADER_HEIGHT = 65.0;
+
 class Team extends StatefulWidget {
   Team({Key key}) : super(key: key);
 
@@ -11,9 +13,15 @@ class Team extends StatefulWidget {
 
 class _TeamState extends State<Team> with SingleTickerProviderStateMixin {
   final user = FirebaseAuth.instance.currentUser;
+  AnimationController _rotationController;
 
   @override
   void initState() {
+    _rotationController = AnimationController(
+      duration: const Duration(milliseconds: 200),
+      vsync: this,
+    );
+
     super.initState();
   }
 
@@ -22,13 +30,7 @@ class _TeamState extends State<Team> with SingleTickerProviderStateMixin {
     return Column(
       children: [
         Container(
-          height: MediaQuery.of(context).size.height - (MediaQuery.of(context).padding.top + 1),
-          margin: EdgeInsets.only(
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0,
-          ),
+          height: MediaQuery.of(context).size.height - (MediaQuery.of(context).padding.top + 1) - TEAM_HEADER_HEIGHT,
           child: NestedScrollView(
             clipBehavior: Clip.antiAlias,
             scrollDirection: Axis.vertical,
@@ -38,14 +40,42 @@ class _TeamState extends State<Team> with SingleTickerProviderStateMixin {
                 SliverOverlapAbsorber(
                   handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
                   sliver: SliverAppBar(
-                    floating: false,
+                    floating: true,
                     primary: true,
-                    toolbarHeight: 0,
-                    collapsedHeight: 0,
-                    expandedHeight: 0,
+                    collapsedHeight: TEAM_HEADER_HEIGHT,
+                    expandedHeight: TEAM_HEADER_HEIGHT,
                     forceElevated: false,
-                    titleSpacing: 0,
-                    backgroundColor: HomeTheme.darkTheme.colorScheme.primaryContainer,
+                    titleSpacing: 2,
+                    backgroundColor: HomeTheme.darkTheme.colorScheme.primary,
+                    title: GestureDetector(
+                      onTap: () {
+                        _rotationController.forward();
+                        _showTeamBottomSheetModal();
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Team Name",
+                            style: HomeTheme.darkTheme.textTheme.bodyText1,
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          RotationTransition(
+                            turns: Tween(begin: 0.0, end: 0.5).animate(_rotationController),
+                            child: RotatedBox(
+                              quarterTurns: 1,
+                              child: Icon(
+                                Icons.arrow_forward_ios_rounded,
+                                color: HomeTheme.darkTheme.textTheme.bodyText1.color,
+                                size: HomeTheme.darkTheme.textTheme.bodyText1.fontSize,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
               ];
@@ -54,6 +84,55 @@ class _TeamState extends State<Team> with SingleTickerProviderStateMixin {
           ),
         ),
       ],
+    );
+  }
+
+  void _showTeamBottomSheetModal() {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+          height: MediaQuery.of(context).size.height - 85,
+          color: Colors.transparent,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primary,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(15.0),
+                topRight: Radius.circular(15.0),
+              ),
+            ),
+            child: TeamList(),
+          ),
+        );
+      },
+      enableDrag: true,
+      isScrollControlled: true,
+      isDismissible: true,
+    ).whenComplete(() {
+      _rotationController.reverse();
+    });
+  }
+}
+
+class TeamList extends StatefulWidget {
+  TeamList({Key key}) : super(key: key);
+
+  @override
+  State<TeamList> createState() => _TeamListState();
+}
+
+class _TeamListState extends State<TeamList> {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        children: [
+          ListTile(
+            title: Text("Old Paint Cans"),
+          ),
+        ],
+      ),
     );
   }
 }
