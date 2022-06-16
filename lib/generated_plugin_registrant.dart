98402ab214ad789ff18cf816c6619c0a7806c031
HadenHiles <haden@howtohockey.com>
2022-06-16 14:02:00 -0400

    Upgrade flutter dependencies

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -18,8 +18,6 @@ import 'package:google_sign_in_web/google_sign_in_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 import 'package:vibration_web/vibration_web.dart';
-import 'package:video_player_web/video_player_web.dart';
-import 'package:wakelock_web/wakelock_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
@@ -37,7 +35,5 @@ void registerPlugins(Registrar registrar) {
   SharedPreferencesPlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
   VibrationWebPlugin.registerWith(registrar);
-  VideoPlayerPlugin.registerWith(registrar);
-  WakelockWeb.registerWith(registrar);
   registrar.registerMessageHandler();
 }
