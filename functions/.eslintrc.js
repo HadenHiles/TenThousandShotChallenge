d976bee6fd3b12d34692609033c4ead462feeb3e
Haden Hiles <hi@hadenhiles.com>
2024-05-09 13:29:21 -0400

    Update firestore rules to match changes in firebase

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: ["tsconfig.json", "tsconfig.dev.json"],
+    project: ["./tsconfig.json", "./tsconfig.dev.json"],
     sourceType: "module",
   },
   ignorePatterns: [
