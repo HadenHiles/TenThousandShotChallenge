57c4671db48a5d657435761ae104c9ddc75a3542
Haden Hiles <hi@hadenhiles.com>
2024-06-19 15:53:09 -0400

    Update node version and ecmascript parsing linting

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
   parserOptions: {
     project: ["./tsconfig.json", "./tsconfig.dev.json"],
     sourceType: "module",
+    ecmaVersion: 8,
   },
   ignorePatterns: [
     "/lib/**/*", // Ignore built files.
