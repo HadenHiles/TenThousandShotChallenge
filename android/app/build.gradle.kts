50c56ac10956c2354101cacb55dfd1609e377f2a
Haden Hiles <hi@hadenhiles.com>
2026-07-27 14:01:56 -0400

    Increase target + compile sdk to api level 36

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,7 +28,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.howtohockey.tenthousandshotchallenge"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -47,7 +47,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         multiDexEnabled = true
         versionCode = flutterVersionCode
         versionName = flutterVersionName
