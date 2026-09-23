e2181ff46e9b8b304c5007fa41c23eda719e50ed
Haden Hiles <hi@hadenhiles.com>
2026-09-23 16:08:08 -0400

    Fix iOS and Android release builds

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -14,6 +14,14 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+
+    plugins.withId("com.android.library") {
+        extensions.configure<com.android.build.api.variant.LibraryAndroidComponentsExtension> {
+            finalizeDsl { libraryExtension ->
+                libraryExtension.compileSdk = 36
+            }
+        }
+    }
 }
 subprojects {
     project.evaluationDependsOn(":app")
